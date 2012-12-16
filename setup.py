0b05134ea7723b8223476c9075779e13a6ac573f
MiCHiLU <djmchl@gmail.com>
2012-12-17 08:23:59 +0900

    Add test runner

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py', 'tests'])
+        raise SystemExit(errno)
+
+class PyTestWithCov(PyTest):
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py', 'tests', '--cov-report=html', '--cov=.', '--pdb'])
+        raise SystemExit(errno)
 
 setup(name="jinja2-precompiler",
       version="0.1",
@@ -36,4 +53,8 @@ setup(name="jinja2-precompiler",
           ]
       },
       zip_safe=False,
+      cmdclass = {
+        'test': PyTest,
+        'cov': PyTestWithCov,
+      },
 )
